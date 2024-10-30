d50a10aac1c6a50f5d52df9d0322777ed1a3dcc6
Omolemo Mashigo <omomashigo@gmail.com>
2024-10-30 18:42:46 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Omolemo Mashigo on 2024/10/30.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
