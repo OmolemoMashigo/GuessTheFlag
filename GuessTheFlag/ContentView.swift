1a0832e6a48b0a83e62fcf33cb3e21683ac4ec7e
Omolemo Mashigo <omomashigo@gmail.com>
2024-10-30 20:35:02 +0200

    Gradients

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,18 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing:0){
-                Color.red
-                Color.blue
-            }
-            
-            Text("content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }.ignoresSafeArea()
+        //LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+//        LinearGradient(stops: [
+//            Gradient.Stop(color: .white, location: 0.45),
+//            Gradient.Stop(color: .black, location: 0.55)
+//            //or use innit
+////            .init(color: .white, location: 0.45),
+////            .init(color: .black, location: 0.55)
+//
+//        
+//        ], startPoint: .top, endPoint: .bottom)
         
+        //RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+        
+        //AngularGradient(colors: [.red, .orange, .yellow,.green,.blue, .purple, .red], center: .center)
+        
+        Text("your content")
+            .frame(maxWidth: .infinity, maxHeight:.infinity)
+            .foregroundStyle(.white)
+            .background(.red.gradient)
     }
 }
 
