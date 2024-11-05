969dcc35463b8c7232041c7997ea2da6cbff2eb1
Omolemo Mashigo <omomashigo@gmail.com>
2024-11-06 00:22:09 +0200

    showing the player's score with an alert

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,17 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showingAlert = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
     var body: some View {
-        Button("show alert"){
-            showingAlert = true
+        ZStack{
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30){
+                VStack{
+                    Text("tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3){ number in
+                    Button{
+                        // flag was tapped
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                    }
+                    
+                }
+            }
         }
-        .alert("important message", isPresented: $showingAlert){
-            Button("Delete", role: .destructive){}
-            Button("Cancel", role: .cancel){}
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action: askQuestion)
         } message: {
-            Text("read this")
+            Text("your score is ???")
         }
+    }
+    
+    func flagTapped(_ number: Int){
+        if number == correctAnswer{
+            scoreTitle = "Correct"
+        }
+        else {
+            scoreTitle = "Incorrect"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0..<2)
     }
     
 }
